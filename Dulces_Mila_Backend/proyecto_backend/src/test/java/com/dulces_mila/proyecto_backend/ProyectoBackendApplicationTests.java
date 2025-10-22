02408d9c4a20c9adcd1534d0fd99c96ad13bb9d2
dvnxeee <dana.miranda@duocuc.cl>
2025-10-22 07:22:10 -0300

    feat(backend): Carga inicial del proyecto backend (Danae)

---
+++ Dulces_Mila_Backend/proyecto_backend/src/test/java/com/dulces_mila/proyecto_backend/ProyectoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulces_mila.proyecto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
